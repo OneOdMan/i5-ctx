3a66e42d1a53a9498fbb4c182ef0c73f6b4f32d4
OneOdMan <namdo2000@gmail.com>
2024-11-22 04:39:27 -0800

    -[Electrical Grid]-
     On branch prototype
     Changes to be committed:
    	modified:   html/electrical-grid.html
    	modified:   routes/electrical-grid.js
    
    - Problems:
    	* div is not being updated. script in electrical-grid.html
    	  doesn't seem to be working

--- routes/electrical-grid.js
+++ routes/electrical-grid.js
@@ -10,8 +10,30 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+let egStatus = "ARMED";
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../html/electrical-grid.html'));
-})
+});
+
+router.get('/opeg-status', (req, res) =>{
+  res.json({status: egStatus});
+  console.log('Sending status: ', egStatus);
+});
+
+router.get('/download/malware', (req,res) => {
+});
+
+router.get('/auth-eg', (req, res) => {
+  const inputText = req.body.textInput.trim();
+
+  if (inputText === "keycode"){
+    egStatus = "DISARMED";
+    console.log(egStatus);
+  } else {
+      res.send('Error: Wrong keycode');
+      console.log('Error, Wrong Keycode');
+  }
+});
 
 module.exports = router;
