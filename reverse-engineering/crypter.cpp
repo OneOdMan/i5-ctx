289bb86ff51f7ef1ed90e88666a62f5cce5f9de1
OneOdMan <namdo2000@gmail.com>
2025-04-11 04:38:12 -0700

    On branch prototype  Changes to be committed: 	modified:   reverse-engineering/box 	modified:   reverse-engineering/creds.txt 	modified:   reverse-engineering/crypter.cpp - Supports both encryption and decryption - Properly verifies credentials - text file is always encrypted after program termination

--- reverse-engineering/crypter.cpp
+++ reverse-engineering/crypter.cpp
@@ -11,58 +11,98 @@ bool Crypter::verify(std::string userIn, std::string passIn) {
     std::getline(ifs, status_line);
     std::cout << "file status: " << status_line << std::endl;
   }
+  ifs.close();
 
   if (status_line == "DECRYPTED") { // Read the rest of the file to verify creds
     // Reading user and pass lines
+    ifs.open("creds.txt", std::ifstream::in);
     std::string temp_user, temp_pass;
     std::getline(ifs, temp_user);
     std::getline(ifs, temp_pass);
+    ifs.close();
     // Checking the creds
     if (userIn != temp_user && passIn != temp_pass) {
       std::cout << "Creds don't match" << std::endl;
+      encode();
       return false;
     }
   } else { // Decrypt the file first before checking the creds.
     decode();
     // Reading user and pass lines
+    ifs.open("creds.txt", std::ifstream::in);
+    std::getline(ifs, status_line);
     std::string temp_user, temp_pass;
     std::getline(ifs, temp_user);
     std::getline(ifs, temp_pass);
+    ifs.close();
     // Checking the creds
     if (userIn != temp_user && passIn != temp_pass) {
       std::cout << "Creds don't match" << std::endl;
+      encode();
       return false;
     }
   }
   std::cout << "Creds match!" << std::endl;
-  ifs.close();
+  encode();
   return true;
 }
 
 void Crypter::encode() {
   // Getting all the lines of text into a string vector
   std::fstream fs;
-  fs.open("creds.txt", std::fstream::in | std::fstream::out);
+  fs.open("creds.txt", std::fstream::in);
   std::vector<std::string> fileLines;
   while (fs.good()) {
     std::string input;
     std::getline(fs, input);
     fileLines.push_back(input);
   }
-
+  fs.close();
+  fs.open("creds.txt", std::fstream::out);
   fs << "ENCRYPTED" << std::endl;
 
-  for (int i = 1; i < fileLines.size(); i++) {
-    fs << encrypt(fileLines.at(i)) << std::endl;
+  for (int i = 1; i < 3; i++) {
+    const std::string in = encrypt(fileLines.at(i));
+    fs << in << std::endl;
   }
   fs.close();
   std::cout << "File is now encrypted!" << std::endl;
 }
 // Encrypting using ROT13
-std::string encrypt(std::string str) {
+std::string Crypter::encrypt(std::string str) {
   std::string newStr;
   for (char &c : str) {
-    c = (c - c + 13) % 26 + c;
+    c += 13;
+    newStr += c;
+  }
+  return newStr;
+}
+
+void Crypter::decode() {
+  // Getting all the lines of text into a string vector
+  std::fstream fs;
+  fs.open("creds.txt", std::fstream::in);
+  std::vector<std::string> fileLines;
+  while (fs.good()) {
+    std::string input;
+    std::getline(fs, input);
+    fileLines.push_back(input);
+  }
+  fs.close();
+  fs.open("creds.txt", std::fstream::out);
+  fs << "DECRYPTED" << std::endl;
+
+  for (int i = 1; i < 3; i++) {
+    fs << decrypt(fileLines.at(i)) << std::endl;
+  }
+  fs.close();
+  std::cout << "File is now decrypted!" << std::endl;
+}
+// Encrypting using ROT13
+std::string Crypter::decrypt(std::string str) {
+  std::string newStr;
+  for (char &c : str) {
+    c -= 13;
     newStr += c;
   }
   return newStr;
